9138254dbb965641172a8bdbfd3a311519f3c721
Jordy McNab <jordymcnab@kryptic.io>
2024-05-28 21:47:47 -0700

    feat: Add Capsule client for API integration

---
+++ packages/react-demo/src/clients/capsuleClient.ts
@@ -0,0 +1,12 @@
+import { Environment } from "@usecapsule/react-sdk";
+import Capsule from "@usecapsule/react-sdk";
+
+// Request a free API key at https://usecapsule.com/beta
+// The API key can be set as an environment variable or hardcoded
+const CAPSULE_API_KEY = "d0b61c2c8865aaa2fb12886651627271";
+
+// Set the environment development and to production when deploying
+const environment = Environment.DEVELOPMENT;
+
+// Create an instance of the Capsule client and export it
+export const capsule = new Capsule(environment, CAPSULE_API_KEY);
